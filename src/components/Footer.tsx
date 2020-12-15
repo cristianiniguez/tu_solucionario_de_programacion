2db6c5961ace1ae558e7698b6b2d68c57d11857c
criniguez <felizpc1@gmail.com>
2020-12-15 15:47:10 -0400

    header and footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,15 @@
+import '../styles/components/Footer.css';
+
 const Footer = () => {
-  return <footer className='Footer'>Footer</footer>;
+  return (
+    <footer className='Footer'>
+      ©{' '}
+      <a href='https://cristianiniguez.github.io' target='_blank' rel='noreferrer'>
+        Cristian iñiguez
+      </a>{' '}
+      - 2020
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,29 @@
+import { Link } from 'react-router-dom';
+
+import '../styles/components/Header.css';
+import logo from '../assets/logo.png';
+
 const Header = () => {
-  return <header className='Header'>Header</header>;
+  return (
+    <header className='Header'>
+      <Link className='Header__link' to='/'>
+        <img className='Header__image' src={logo} alt='logo' />
+      </Link>
+      <nav className='Header__navbar'>
+        <ul>
+          <li>
+            <Link to='/'>Inicio</Link>
+          </li>
+          <li>
+            <Link to='/subjects'>Materias</Link>
+          </li>
+          <li>
+            <Link to='/about'>Acerca de</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
