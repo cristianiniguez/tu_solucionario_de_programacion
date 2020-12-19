3236bbb49d7166e9c9db724a9dd62dda6e3a3fff
criniguez <felizpc1@gmail.com>
2020-12-19 17:48:23 -0400

    home: hero added

---
+++ src/components/Hero.tsx
@@ -0,0 +1,24 @@
+import '../styles/components/Hero.css';
+import herobg from '../assets/hero.jpg';
+
+const Hero = () => {
+  return (
+    <section
+      className='Hero'
+      style={{
+        background: `linear-gradient(var(--hero-shadow), var(--hero-shadow)), url(${herobg})`,
+        backgroundSize: 'cover',
+      }}
+    >
+      <h1 className='Hero__title'>
+        Bienvenido a <br />
+        <code className='text--blue'>
+          tu
+        </code> <code className='text--yellow'>solucionario</code>{' '}
+        <code className='text--red'>de</code> <code className='text--green'>programación</code>
+      </h1>
+    </section>
+  );
+};
+
+export default Hero;
