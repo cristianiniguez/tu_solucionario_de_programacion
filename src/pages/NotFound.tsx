cf3d4673d0ade5c03c1536720de0dccae5402923
criniguez <felizpc1@gmail.com>
2020-12-20 20:28:36 -0400

    notfound page styles

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,18 @@
 import { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
+
+import '../styles/pages/NotFound.scss';
 
 const NotFound: FunctionComponent = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className='NotFound'>
+      <section>
+        <h2>404</h2>
+        <p>Lo que estás buscando no se pudo encontrar</p>
+        <Link to='/'>Ir al Home</Link>
+      </section>
+    </div>
+  );
 };
 
 export default NotFound;
