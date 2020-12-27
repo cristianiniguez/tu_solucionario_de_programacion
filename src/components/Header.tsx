3f3c504394a594f7cbd78474750e4fc07bf2e94f
criniguez <felizpc1@gmail.com>
2020-12-27 18:13:00 -0400

    header logo image responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,17 @@ import Searchbox from './Searchbox';
 import Navbar from './Navbar';
 
 import '../styles/components/Header.scss';
+import minilogo from '../assets/minilogo.png';
 import logo from '../assets/logo.png';
 
 const Header: FunctionComponent = () => {
   return (
     <header className='Header'>
       <Link className='Header__link' to='/'>
-        <img className='Header__image' src={logo} alt='logo' />
+        <picture className='Header__image'>
+          <source media='(min-width: 1024px)' srcSet={logo} />
+          <img src={minilogo} alt='logo' />
+        </picture>
       </Link>
       <Searchbox />
       <Navbar />
