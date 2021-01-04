a334fa2e90016814db7670729578ecf13e42bc7d
criniguez <felizpc1@gmail.com>
2021-01-04 18:48:37 -0400

    PostMenu styles improved

--- src/components/PostMenu.tsx
+++ src/components/PostMenu.tsx
@@ -1,19 +1,31 @@
 import { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import Markdown from 'markdown-to-jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 import { IPost } from '../data/posts';
+import { getSubjectByEndpoint } from '../data/subjects';
 import '../styles/components/PostMenu.scss';
+
+type PostMenuContainerProps = {
+  hovercolor: string | undefined;
+};
+
+const PostMenuContainer = styled.ul<PostMenuContainerProps>`
+  --hover-color: ${(props) => props.hovercolor || '#000'};
+`;
 
 type PostMenuProps = {
   post: IPost;
 };
 
 const PostMenu: FunctionComponent<PostMenuProps> = ({ post }) => {
+  const matchedSubject = getSubjectByEndpoint(post.subject);
+
   return (
-    <ul className='PostMenu'>
+    <PostMenuContainer hovercolor={matchedSubject?.brandColor} className='PostMenu normalized'>
       {post.pages.map((page, i) => (
         <li key={i}>
           <Link to={`/post/${post.NO_ID_FIELD}/${i}`}>
@@ -22,7 +34,7 @@ const PostMenu: FunctionComponent<PostMenuProps> = ({ post }) => {
           </Link>
         </li>
       ))}
-    </ul>
+    </PostMenuContainer>
   );
 };
 
