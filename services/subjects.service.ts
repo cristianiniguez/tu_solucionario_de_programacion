9c18a3a7d9888d4b650ea99bf4c5c70a611d6ad8
cristianiniguez <felizpc1@gmail.com>
2021-07-19 20:25:44 -0400

    type warnings fixed

--- services/subjects.service.ts
+++ services/subjects.service.ts
@@ -1,5 +1,10 @@
 import NotionLib from '@/lib/notion';
 import { TSubject } from '@/types/common';
+import {
+  RichTextPropertyValue,
+  RichTextText,
+  TitlePropertyValue,
+} from '@notionhq/client/build/src/api-types';
 
 class SubjectsService {
   private notionDB: NotionLib;
@@ -10,15 +15,23 @@ class SubjectsService {
 
   async getAll(): Promise<TSubject[]> {
     const { results } = await this.notionDB.getSubjectsData();
-    return results.map(({ id, properties }) => ({
-      id,
-      name: properties.name.title[0].text.content,
-      slug: properties.slug.rich_text[0].text.content,
-      description: properties.description.rich_text[0].text.content,
-      brandColor: properties.brandColor.rich_text[0].text.content,
-      textColor: properties.textColor.rich_text[0].text.content,
-      icon: properties.icon.rich_text[0].text.content,
-    }));
+    return results.map(({ id, properties }) => {
+      return {
+        id,
+        name: ((properties.name as TitlePropertyValue).title[0] as RichTextText).text.content,
+        slug: ((properties.slug as RichTextPropertyValue).rich_text[0] as RichTextText).text
+          .content,
+        description: (
+          (properties.description as RichTextPropertyValue).rich_text[0] as RichTextText
+        ).text.content,
+        brandColor: ((properties.brandColor as RichTextPropertyValue).rich_text[0] as RichTextText)
+          .text.content,
+        textColor: ((properties.textColor as RichTextPropertyValue).rich_text[0] as RichTextText)
+          .text.content,
+        icon: ((properties.icon as RichTextPropertyValue).rich_text[0] as RichTextText).text
+          .content,
+      };
+    });
   }
 }
 
