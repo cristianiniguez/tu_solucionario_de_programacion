3ecd4a36cd2aff757f2e6c022c832ed765acdca6
cristianiniguez <felizpc1@gmail.com>
2022-10-12 18:12:04 -0400

    task/TSDP-1 Imports sorted on Layout

--- apps/ui-pre/src/components/Layout.tsx
+++ apps/ui-pre/src/components/Layout.tsx
@@ -1,7 +1,7 @@
+import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import '../styles/components/Layout.scss';
-import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
